7dd9bcf650bf0eb5460bf55e10cde54551937e1e
GG0323 <128014999+GG0323@users.noreply.github.com>
2025-11-28 14:23:10 +0900

    [Bronze IV] Title: Body Mass Index, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6825. Body Mass Index/Body Mass Index.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	double w, h;
+	cin >> w >> h;
+	double bmi = w / (h * h);
+	if (bmi > 25)
+		cout << "Overweight";
+	else if (bmi > 18.5 && bmi <= 25)
+		cout << "Normal weight";
+	else
+		cout << "Underweight";
+}
