87d9bbcbfbd9a185d4a088fdd6ad8e7df3abd990
GG0323 <128014999+GG0323@users.noreply.github.com>
2025-11-28 14:38:50 +0900

    [Bronze III] Title: 앵그리 창영, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, h, tmp;
+    cin >> n >> m >> h;
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        tmp* tmp <= m * m + h * h ? cout << "DA\n" : cout << "NE\n";
+    }
+}
