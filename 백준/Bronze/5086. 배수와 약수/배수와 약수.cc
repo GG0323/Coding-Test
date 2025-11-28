5efee911725e1a9d1eca4d109288dffacfb2ddaf
GG0323 <128014999+GG0323@users.noreply.github.com>
2025-11-28 14:39:57 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int x, y;
+
+	while (1) {
+		cin >> x >> y;
+
+		if (x == 0 && y == 0)
+			break;
+
+		if (y % x == 0)
+			cout << "factor" << '\n';
+
+		else if (x % y == 0) 
+			cout << "multiple" << '\n';
+
+		else 
+			cout << "neither" << '\n';
+    }
+}
