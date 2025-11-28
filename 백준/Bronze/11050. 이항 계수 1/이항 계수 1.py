c2422eeb84a543b92c414eab4019631885388fea
GG0323 <128014999+GG0323@users.noreply.github.com>
2025-11-28 14:57:28 +0900

    [Bronze I] Title: 이항 계수 1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,6 @@
+n, k = map(int, input().split())
+pascal = [[1]*(i+1) for i in range(n+1)]
+for i in range(2, n+1):
+    for j in range(1,i):
+        pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+print(pascal[n][k])
